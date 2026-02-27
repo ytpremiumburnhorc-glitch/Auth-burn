95a5df68185b9ef88e53c8e25a0e6655626cd50e
ytpremiumburnhorc-glitch <ytpremium.burnhorc@gmail.com>
2026-02-27 22:31:08 +0800

    Implement logout handler to clear session cookie

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,1 +1,16 @@
+import { serialize } from "cookie";
 
+export default function handler(req, res) {
+  res.setHeader(
+    "Set-Cookie",
+    serialize("token", "", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      path: "/",
+      expires: new Date(0),
+    })
+  );
+
+  res.status(200).json({ message: "Logged out" });
+}
